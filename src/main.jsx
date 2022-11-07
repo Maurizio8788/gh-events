c2f44a9419882025d9a0ea0f9cd2ec982e6fcbb9
Maurizio Strazzullo <maurizio.strazzullo@alten.it>
2022-11-07 22:47:57 +0100

    chenged comment

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// this is important
+// this is very important
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
